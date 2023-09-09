ca0ad132d6cba5d2d48617183868c3aed5fe8bf9
Artemskyba <artemgennadievich@icloud.com>
2023-09-09 17:49:43 +0300

    finished tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+const player = new Player('vimeo-player',);
+const savedTimeline = localStorage.getItem('videoplayer-current-time');
+
+if (savedTimeline) {
+  player.setCurrentTime(savedTimeline);
+}
+
+player.on('timeupdate', throttle(getTimeupdate, 1000));
+
+function getTimeupdate(e) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
+};
